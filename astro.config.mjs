8c197368b061a112921b19ad729efec1e47e3231
James Conroy-Finn <git@invetica.co.uk>
2023-07-14 15:59:01 +0100

    Add the all important Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,18 +3,21 @@ import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 
 // FIXME Change your site before deploying!
+import vercel from "@astrojs/vercel/serverless";
 const site = process.env.ASTRO_SITE ?? "https://example.com/";
+
 
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
   site: site,
   experimental: {
-    assets: true,
+    assets: true
   },
   image: {
-    service: sharpImageService(),
+    service: sharpImageService()
   },
   integrations: [tailwind(), mdx()],
   trailingSlash: "never",
+  adapter: vercel()
 });
